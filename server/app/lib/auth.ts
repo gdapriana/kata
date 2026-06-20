76e36c45f2bea9ef813f3b7c7763944b472af33c
gdapriana <gedeapriana36@gmail.com>
2026-06-20 11:24:40 +0200

    settup better auth

---
+++ server/app/lib/auth.ts
@@ -0,0 +1,18 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prismaClient } from "../database/db.js";
+
+export const auth = betterAuth({
+    database: prismaAdapter(prismaClient, {
+        provider: "postgresql", 
+    }),
+    emailAndPassword: { 
+        enabled: true, 
+    }, 
+    socialProviders: { 
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID as string, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+        }, 
+    }, 
+});
--- server/app/server.ts
+++ server/app/server.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import cors from "cors";
+import { toNodeHandler } from "better-auth/node";
 import cookieParser from "cookie-parser";
+import { auth } from "./lib/auth.js";
 
 export const app = express();
+app.all('/api/auth/{*any}', toNodeHandler(auth));
 app.use(express.json());
 
 app.get("/", (req, res, next) => {
